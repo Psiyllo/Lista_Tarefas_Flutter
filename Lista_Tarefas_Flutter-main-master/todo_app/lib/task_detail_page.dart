0c359e4e47c878e705faa5e236d14db2e40d4b9a
Psiyllo <paulocesaralcabral@gmail.com>
2024-06-13 15:37:38 -0300

    resolving errors

--- Lista_Tarefas_Flutter-main-master/todo_app/lib/task_detail_page.dart
+++ Lista_Tarefas_Flutter-main-master/todo_app/lib/task_detail_page.dart
@@ -226,4 +226,17 @@ class _TaskDetailPageState extends State<TaskDetailPage> {
     );
   }
 
-  
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2101),
+    );
+    if (pickedDate != null && pickedDate != _selectedDate) {
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    }
+  }
+}
